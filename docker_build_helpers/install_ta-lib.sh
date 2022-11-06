772acedf9bf89a78636c7d1963b3cc0dbc195039
jamesbaber1 <31859220+jamesbaber1@users.noreply.github.com>
2022-11-06 15:12:40 -0600

    - specified build

--- docker_build_helpers/install_ta-lib.sh
+++ docker_build_helpers/install_ta-lib.sh
@@ -10,7 +10,7 @@ if [ ! -f "${INSTALL_LOC}/lib/libta_lib.a" ]; then
   tar zxvf ta-lib-0.4.0-src.tar.gz
   cd ta-lib \
   && sed -i.bak "s|0.00000001|0.000000000000000001 |g" src/ta_func/ta_utility.h \
-  && ./configure --prefix=${INSTALL_LOC}/ \
+  && ./configure --build=aarch64-unknown-linux-gnu --prefix=${INSTALL_LOC}/ \
   && make \
   && which sudo && sudo make install || make install \
   && echo "export LD_LIBRARY_PATH=/usr/local/lib" >> /root/.bashrc
